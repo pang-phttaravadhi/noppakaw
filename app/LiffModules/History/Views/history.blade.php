6509f46ebce0caa824a3208f410c4d87ef0f8011
pangg <pangpaweenapoktueng@gmail.com>
2019-08-02 11:57:29 +0700

    ออกแบบแก้ไขหน้าประวัติการสั่งซื้อและหน้าfrom

--- app/LiffModules/History/Views/history.blade.php
+++ app/LiffModules/History/Views/history.blade.php
@@ -1,44 +1,31 @@
 @extends('liff-layout')
 @section('title','history')
 @section('content' )
-<div class="container">
+<div>
+    <h4>รายการใบสั่งซื้อ</h4>
     <div class="panel panel-default">
-        <div class="panel-heading">ประวัติการสั่งซื้อสินค้า</div>
+        <div class="panel-heading text-center"><strong>รายการสินค้า</strong></div>
         <div class="panel-body">
-            <a href="https://i.pinimg.com/736x/38/d2/b5/38d2b5994f4df4daf4bb651d1bbbb85a.jpg">
-                <img src="https://i.pinimg.com/736x/38/d2/b5/38d2b5994f4df4daf4bb651d1bbbb85a.jpg" alt="Nature" style="width:15%">
-                <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                    <strong class="d-block text-gray-dark">001</strong>
-                    <strong class="d-block text-gray-dark">ชุดไทยจักรพรรดิ สีแดงเลือดนก</strong>
-                    <strong class="d-block text-gray-dark">3000 บาท</strong>
-                </p>
-                <a class="btn btn-info" href="/liff/historyfrom">รีวิวสินค้า</a>
+            <table style="width:100%">
+                @for($i=0;$i<2;$i++)
+                <tr>
+                    <td colspan="2" style="margin-bottom:5px;padding:10px 0">
+                        <a href="https://i.pinimg.com/736x/38/d2/b5/38d2b5994f4df4daf4bb651d1bbbb85a.jpg">
+                            <img src="https://i.pinimg.com/736x/38/d2/b5/38d2b5994f4df4daf4bb651d1bbbb85a.jpg"  alt="Nature" style="width:20%;float:left;">
+                        </a>
+                        <div style="width:78%;float:right;margin-left:5px;">
+                            <p style="margin:3px;"><strong>เสื้อผ้าปปปปปปปปปปปปปปปป</strong></p>
+                            <p style="margin:3px;">ราคา : 200 บาท</p>
+                            <p style="margin:3px;">สถานะ จัดส่งเรียบร้อยแล้ว</p>
+                                <a href="/liff/historyfrom" style='margin:25px 0;' class="btn btn-wheat btn-block">ใบสั่งซื้อสินค้า</a>
+                        </div>
+                    </td>
+                </tr>
+                @endfor
+            </table>
         </div>
-        <div class="panel-body">
-            <a href="http://entertain.teenee.com/series/img6/1023314.jpg">
-                <img src="http://entertain.teenee.com/series/img6/1023314.jpg" style="width:15%">
-                <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                    <strong class="d-block text-gray-dark">002</strong>
-                    <strong class="d-block text-gray-dark">ชุดไทยเรือนต้น สีชมพู</strong>
-                    <strong class="d-block text-gray-dark">2500 บาท</strong>
-                </p>
-                <a class="btn btn-info" href="/liff/historyfrom">รีวิวสินค้า</a>
-
-        </div>
-        <div class="panel-body">
-            <a href="https://th-live-02.slatic.net/original/0f76c2267524f0d284a2a4a1905635f9.jpg">
-                <img src="https://th-live-02.slatic.net/original/0f76c2267524f0d284a2a4a1905635f9.jpg" style="width:15%">
-                <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                    <strong class="d-block text-gray-dark">003</strong>
-                    <strong class="d-block text-gray-dark">ชุดไทยจิตรลดา สีครีม</strong>
-                    <strong class="d-block text-gray-dark">2500 บาท</strong>
-                </p>
-                <a class="btn btn-info" href="/liff/historyfrom">รีวิวสินค้า</a>
-
-        </div>
-    </div>
+    </div>    
 </div>
-
 
 @endsection
 
--- app/LiffModules/History/Views/historyfrom.blade.php
+++ app/LiffModules/History/Views/historyfrom.blade.php
@@ -1,22 +1,78 @@
 @extends('liff-layout')
 @section('title')
 @section('content' )
-<label>กรุณาป้อนความคิดเห็นด้วยค่ะ</label>
-    <div class="form-group">
-        <label>ความคิดเห็น:</label>
-        <textarea class="form-control" rows="2" id="comment"></textarea>
-    </div>
-    <p>ใส่รูปภาพหลักฐานการโอน</p>
-    <div class="row">
-        <div class="col-md-4">
-            <button type="button" data-callback="uploaded" data-ext="jpg,png,jpeg" class="btn btn-primary upload">อัพโหลดรูปภาพ</button>
-                <input type="hidden" value="{{ isset($payment) ? $payment->image:''}}" name="image" id="image"/>
-                    <div class="thumbnail">
-                            <img src="{{ isset($payment) ? $payment->image:''}}" id="display-image" alt="Lights" style="width:100%">
-                    </div>
+<div>
+    <h4>ทำการสั่งซื้อ</h4>
+    <div class="panel panel-default">
+        <div class="panel-heading text-center"><strong>ที่อยู่ในการจัดส่ง</strong></div>
+        <div class="panel-body">
+            <table style="width:100%">
+                <label>ชื่อ</label>
+                <input type="text" class="form-control"  name="" value="">
+                <label>ที่อยู่ในการจัดส่ง</label>
+                <input type="text" class="form-control"  name="" value="">
+                <label>เบอร์โทร</label>
+                <input type="text" class="form-control"  name="" value="">
+                
+          <tr>
+              <td colspan="2" style="margin-bottom:5px;padding:10px 0">
+                  <a href="https://i.pinimg.com/736x/38/d2/b5/38d2b5994f4df4daf4bb651d1bbbb85a.jpg">
+                    <img src="https://i.pinimg.com/736x/38/d2/b5/38d2b5994f4df4daf4bb651d1bbbb85a.jpg"  alt="Nature" style="width:20%;float:left;">
+                  </a>
+                  <div style="width:78%;float:right;margin-left:5px;">
+                    <p style="margin:3px;"><strong>เสื้อผ้าปปปปปปปปปปปปปปปป</strong></p>
+                    <p style="margin:3px;">ราคา : 200 บาท</p>
+                    <p style="margin:3px;">จำนวน : <input class="text-center" style="width:85px;padding:5px;" type="number" value="0"></p>
+                  </div>
+              </td>
+          </tr>
+          
+          <tr>
+            <td class="text-right" style="padding-top:10px;margin-bottom:2px;border-top:3px double #ccc;font-size:15px;">
+              <strong>จำนวนทั้งหมด :</strong>
+            </td>
+            <td class="text-right" style="padding-top:10px;margin-bottom:2px;border-top:3px double #ccc;width:120px;font-size:15px;">
+              <strong>1 ชิ้น</strong>
+            </td>
+          </tr>
+          <tr>
+            <td class="text-right" style="padding-top:10px;margin-bottom:2pxfont-size:15px;">
+              <strong>ราคารวม :</strong>
+            </td>
+            <td class="text-right" style="padding-top:10px;margin-bottom:2px;font-size:15px;">
+              <strong>200 บาท</strong>
+            </td>
+          </tr>
+          <tr>
+            <td class="text-right" style="padding-top:10px;margin-bottom:2px;font-size:15px;">
+              <strong>ส่วนลด :</strong>
+            </td>
+            <td class="text-right" style="padding-top:10px;margin-bottom:2px;font-size:15px;">
+              <strong>0 บาท</strong>
+            </td>
+          </tr>
+          <tr>
+            <td class="text-right" style="padding-top:10px;padding-bottom:10px;margin-bottom:2px;font-size:15px;">
+              <strong>ค่าขนส่ง :</strong>
+            </td>
+            <td class="text-right" style="padding-top:10px;padding-bottom:10px;margin-bottom:2px;font-size:15px;">
+              <strong>50 บาท</strong>
+            </td>
+          </tr>
+          <tr>
+            <td class="text-right" style="border-top:3px double #ccc;border-bottom:3px double #ccc;padding:10px 0;margin-bottom:2px;font-size:19px;color:#b00">
+              <strong>ยอดสุทธิ :</strong>
+            </td>
+            <td class="text-right" style="border-top:3px double #ccc;border-bottom:3px double #ccc;padding:10px 0;margin-bottom:2px;font-size:19px;color:#b00">
+              <strong>250 บาท</strong>
+            </td>
+          </tr>
+            </table>
+            
+            <a href="/liff/history" style='margin:25px 0;' class="btn btn-primary btn-block">กลับ</a>
         </div>
-    </div>    
-    <button type="submit" class="btn btn-primary">บันทึก</button>
-    <a href="/liff/history" class="btn btn-info">กลับ</a>
+    </div>
+</div>
+    
         
 @endsection
