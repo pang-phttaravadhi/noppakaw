5afa3d5aebbd848cc9b97ed0fabc5b415e36a9d2
pangg <pangpaweenapoktueng@gmail.com>
2019-09-02 09:46:35 +0700

    ลองทำหน้า payment ยังไม่สำเร็จ

--- app/LiffModules/Paymentliff/PaymentliffController.php
+++ app/LiffModules/Paymentliff/PaymentliffController.php
@@ -14,9 +14,12 @@ class PaymentliffController extends Controller
 {
     public function index()
     {
-        
-
-        return view('payy::Paymentliff');
+        $customer= CurrentUser::user();
+        $payments = DB ::table('orders')
+        ->select('orders.*')
+        ->first();
+        print_r($payments);exit;
+        return view('payy::Paymentliff',compact('payments','customer'));
     	
     }
     public function paymentlifffrom()
@@ -24,75 +27,73 @@ class PaymentliffController extends Controller
         return view('payyfrom::Paymentlifffrom');
     	
     }
-    public function store(Request $request)
-    {
-        $customers= CurrentUser::user();
-        $payments = DB ::table('payment');
-        $order_id = $request->get('order_id');
-        $bank_type = $request->get('bank_type');
-        $price_net = $request->get('price_net'); 
-        $image = $request->get('image');
-        if(!empty($order_id) && !empty($bank_type) && !empty($price_net) && !empty($image))
-         {
-           $payments = DB::table('payment')
-           ->insert([
-                'order_id' =>$order_id,
-                'bank_type' =>$bank_type,
-                '$price_net' =>$price_net,
-                '$image' =>$image,
-            ]);
+    // public function store(Request $request)
+    // {
+    //     $customers= CurrentUser::user();
+    //     $payments = DB ::table('payment');
+    //     $order_id = $request->get('order_id');
+    //     $bank_type = $request->get('bank_type');
+    //     $price_net = $request->get('price_net'); 
+    //     $image = $request->get('image');
+    //     if(!empty($order_id) && !empty($bank_type) && !empty($price_net) && !empty($image))
+    //      {
+    //        $payments = DB::table('payment')
+    //        ->insert([
+    //             'order_id' =>$order_id,
+    //             'bank_type' =>$bank_type,
+    //             '$price_net' =>$price_net,
+    //             '$image' =>$image,
+    //         ]);
             //print_r($payments);exit;
-            return MyResponse::success('ระบบได้บันทึกข้อมูลเรียบร้อยแล้ว','/paymentliff');
-        }else{
-            return MyResponse::error('กรุณาป้อนข้อมูลให้ครบค่ะ');
-        }
-    	return view('payyfrom::Paymentlifffrom',compact('payments'));
+    //         return MyResponse::success('ระบบได้บันทึกข้อมูลเรียบร้อยแล้ว','/paymentliff');
+    //     }else{
+    //         return MyResponse::error('กรุณาป้อนข้อมูลให้ครบค่ะ');
+    //     }
+    // 	return view('payyfrom::Paymentlifffrom',compact('payments'));
 
-    }
-    public function update($pay_id,Request $request)
-    {
-        if(is_numeric($pay_id))
-        {
-            $order_id = $request->get('order_id');
-            $created_at = $request->get('created_at');
-            $bank_type = $request->get('bank_type');
-            $price_net = $request->get('price_net');
-            $image = $request->get('image');
+    // }
+    // public function update($pay_id,Request $request)
+    // {
+    //     if(is_numeric($pay_id))
+    //     {
+    //         $order_id = $request->get('order_id');
+    //         $created_at = $request->get('created_at');
+    //         $bank_type = $request->get('bank_type');
+    //         $price_net = $request->get('price_net');
+    //         $image = $request->get('image');
 
-            if(!empty($order_id) 
-            && !empty($image) 
-            && is_numeric( $created_at) 
-            && is_numeric($bank_type) 
-            && is_numeric($price_net) 
-            )
-            {
-                $payments = DB ::table('payment')
-                ->select('payment.*','orders.order_id','order.price_net')
-                ->leftJoin('orders','payment.order_id','orders.order_id')
-                ->where('order_id',$order_id)
-                ->whereNull('payment.deleted_at')
-                ->get();
-                if(!empty($payment)){
-                    return MyResponse::error('ขออภัยข้อมูลนี้มีอยู่ในระบบแล้ว');
-                }
-                DB::table('payment')
-                ->where('pay_id',$pay_id)
-                ->update([
-                    'order_id' =>$order_id,
-                    'created_at' =>$created_at,
-                    'bank_type' =>$bank_type,
-                    'price_net'=>$price_net,
-                    'image'=>$image,
-                    'updated_at'=>date('Y-m-d H:i:s')
-                ]);
-                return MyResponse::success('ระบบได้บันทึกข้อมูลเรียบร้อยแล้วค่ะ','/payment');
-            }else{ 
-                return MyResponse::error('กรุณาป้อนข้อมุลให้ครบค่ะ');
-            }
-        }
-        return MyResponse::error('ป้อนข้อมูลไม่ถูกต้องค่ะ');
-    }
+    //         if(!empty($order_id) 
+    //         && !empty($image) 
+    //         && is_numeric( $created_at) 
+    //         && is_numeric($bank_type) 
+    //         && is_numeric($price_net) 
+    //         )
+    //         {
+    //             $payments = DB ::table('payment')
+    //             ->select('payment.*','orders.order_id','order.price_net')
+    //             ->leftJoin('orders','payment.order_id','orders.order_id')
+    //             ->where('order_id',$order_id)
+    //             ->whereNull('payment.deleted_at')
+    //             ->get();
+    //             if(!empty($payment)){
+    //                 return MyResponse::error('ขออภัยข้อมูลนี้มีอยู่ในระบบแล้ว');
+    //             }
+    //             DB::table('payment')
+    //             ->where('pay_id',$pay_id)
+    //             ->update([
+    //                 'order_id' =>$order_id,
+    //                 'created_at' =>$created_at,
+    //                 'bank_type' =>$bank_type,
+    //                 'price_net'=>$price_net,
+    //                 'image'=>$image,
+    //                 'updated_at'=>date('Y-m-d H:i:s')
+    //             ]);
+    //             return MyResponse::success('ระบบได้บันทึกข้อมูลเรียบร้อยแล้วค่ะ','/payment');
+    //         }else{ 
+    //             return MyResponse::error('กรุณาป้อนข้อมุลให้ครบค่ะ');
+    //         }
+    //     }
+    //     return MyResponse::error('ป้อนข้อมูลไม่ถูกต้องค่ะ');
+    // }
     
 }
-
-
--- app/LiffModules/Paymentliff/Views/paymentlifffrom.blade.php
+++ app/LiffModules/Paymentliff/Views/paymentlifffrom.blade.php
@@ -5,31 +5,31 @@
 <p>กรุณาป้อนข้อมูล</p>
 <form action="/liff/paymentlifffrom/{{isset($payments)?$cust->cust_id:''}}" 
         class="form-ajax" method="POST"> 
-            <input type="hidden" value="POST" name="_mathods">
+            <input type="hidden" value="post" name="_mathods">
 </form>
 <div class="form-group">
   <label>เลขใบสั่งซื้อ:</label>
-    <input type="text" class="form-control"  name="order_id" value="{{isset($payments)?$payment->order_id:''}}">
+    <input type="text" class="form-control"  name="order_id" value="{{isset($payments)?$payments->order_id:''}}">
 </div>
 <div class="form-group">
   <label>วัน/เดือน/ปี ที่โอน:</label>
-    <input type="date" class="form-control"  name="created_at " value="{{isset($payments)?$payment->created_at:''}}">
+    <input type="date" class="form-control"  name="created_at " value="{{isset($payments)?$payments->created_at:''}}">
 </div>
 <div class="form-group">
   <label>ธนาคาร:</label>
-    <input type="text" class="form-control"  name="bank_type" value="{{isset($payments)?$payment->bank_type:''}}">
+    <input type="text" class="form-control"  name="bank_type" value="{{isset($payments)?$payments->bank_type:''}}">
 </div>
 <div class="form-group">
   <label>จำนวนเงิน:</label>
-    <input type="text" class="form-control"  name="price_net" value="{{isset($payments)?$payment->price_net:''}}">
+    <input type="text" class="form-control"  name="price_net" value="{{isset($payments)?$payments->price_net:''}}">
 </div>
 <p>ใส่รูปภาพหลักฐานการโอน</p>
 <div class="row">
   <div class="col-md-4">
     <button type="button" data-callback="uploaded" data-ext="jpg,png,jpeg" class="btn btn-primary upload">อัพโหลดรูปภาพ</button>
-    <input type="hidden" value="{{ isset($payments)?$payment->image:''}}" name="image" id="image"/>
+    <input type="hidden" value="{{ isset($payments)?$payments->image:''}}" name="image" id="image"/>
       <div class="thumbnail">
-        <img src="{{ isset($payments)?$payment->image:''}}" id="display-image" alt="Lights" style="width:100%">
+        <img src="{{ isset($payments)?$payments->image:''}}" id="display-image" alt="Lights" style="width:100%">
       </div>
   </div>
 </div>      
